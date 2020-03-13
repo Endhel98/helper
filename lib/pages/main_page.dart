4030aaa29c83dff985956cb3547f87966c6be852
endhellopes <endheltog@gmail.com>
2020-03-13 13:21:59 -0300

    Funções para manipular o json

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:helper/pages/class_page.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -7,6 +11,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,5 +48,25 @@ class _MainPageState extends State<MainPage> {
       ),
       extendBodyBehindAppBar: true,
     );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }

f0866ecf0101f693d6b0945f9cdb43b061ff9f22
endhellopes <endheltog@gmail.com>
2020-03-24 12:07:38 -0300

    Dismissible

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:helper/pages/class_page.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -7,9 +10,20 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  //Map<String, dynamic> _lastRemoved;
-  //int _lastRemovedPos;
-  List toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+  List _toDoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,96 +60,133 @@ class _MainPageState extends State<MainPage> {
       body: ListView.builder(
         padding: EdgeInsets.only(top: 10.0),
         itemBuilder: buildItem,
-        itemCount: toDoList.length,
+        itemCount: _toDoList.length,
       ),
     );
   }
 
   Widget buildItem(context, index) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(
-                      Icons.school,
-                      color: Colors.lightGreen,
-                    ),
-                  ),
-                  Text(
-                    toDoList[index]["class"],
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.red,
-                    ),
-                  ),
-                  Text(
-                    toDoList[index]["professor"],
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(
-                      Icons.local_library,
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  Text(
-                    toDoList[index]["classRom"],
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(
-                      Icons.watch_later,
-                      color: Colors.purple,
-                    ),
-                  ),
-                  Text(
-                    toDoList[index]["hour"],
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          ],
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
         ),
       ),
+      direction: DismissDirection.startToEnd,
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.school,
+                        color: Colors.lightGreen,
+                      ),
+                    ),
+                    Text(
+                      _toDoList[index]["class"],
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.red,
+                      ),
+                    ),
+                    Text(
+                      _toDoList[index]["professor"],
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.local_library,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Text(
+                      _toDoList[index]["classRom"],
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 5, left: 6, top: 5),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Icon(
+                        Icons.watch_later,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    Text(
+                      _toDoList[index]["hour"],
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Disciplina \"${_lastRemoved['title']}\" removida"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
@@ -145,6 +196,26 @@ class _MainPageState extends State<MainPage> {
       MaterialPageRoute(builder: (context) => ClassPage()),
     );
 
-    toDoList = recList;
+    if (recList != null) _toDoList = recList;
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
   }
 }

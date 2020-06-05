b9b9718e57e06d5d6b6f0a7884e987c48a39e2ce
endhel <endheltog@gmail.com>
2020-06-05 00:28:05 -0300

    Changed Design HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:helper/animations/growTransition.dart';
 import 'package:helper/functionsJson/functions.dart';
 import 'package:helper/pages/class_info_page.dart';
@@ -26,9 +27,14 @@ class _MainPageState extends State<HomePage>
 
     controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 1),
+      duration: Duration(seconds: 5),
     );
-    animation = Tween<double>(begin: 0, end: 250).animate(controller);
+    animation = CurvedAnimation(parent: controller, curve: Curves.elasticOut)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed)
+          controller.reverse();
+        else if (status == AnimationStatus.dismissed) controller.forward();
+      });
 
     controller.forward();
 
@@ -48,52 +54,47 @@ class _MainPageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         centerTitle: true,
-        elevation: 0.8,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "HELPER",
-              style: TextStyle(
-                color: Colors.deepPurpleAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
-            ),
-            SizedBox(
-              width: 5,
-            ),
-            Icon(
-              Icons.import_contacts,
-              color: Colors.deepPurpleAccent,
-              size: 35,
-            )
-          ],
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Image.asset("images/logo.png"),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.deepPurpleAccent,
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        child: Icon(
+          Icons.add_circle,
+          color: Colors.white,
+          size: 50,
+        ),
+        onPressed: () {
+          _showClassPage();
+        },
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.jpg"),
+            fit: BoxFit.fill,
           ),
-          onPressed: () {
-            _showClassPage();
-          }),
-      body: _toDoList.isEmpty
-          ? GrowTransition(
-              child: EmptyList(),
-              animation: animation,
-            )
-          : ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemBuilder: buildItem,
-              itemCount: _toDoList.length,
-            ),
+        ),
+        child: _toDoList.isEmpty
+            ? GrowTransition(
+                child: EmptyList(),
+                animation: animation,
+              )
+            : ListView.builder(
+                padding: EdgeInsets.only(top: 100.0),
+                itemBuilder: buildItem,
+                itemCount: _toDoList.length,
+              ),
+      ),
     );
   }
 
@@ -114,14 +115,14 @@ class _MainPageState extends State<HomePage>
       child: Dismissible(
         key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
         background: Container(
-          color: Colors.red.withOpacity(0.8),
+          color: Colors.transparent.withOpacity(0.3),
           child: Align(
             alignment: Alignment(-0.9, 0.0),
             child: Icon(Icons.delete, color: Colors.white),
           ),
         ),
         secondaryBackground: Container(
-          color: Colors.red.withOpacity(0.8),
+          color: Colors.transparent.withOpacity(0.3),
           child: Align(
             alignment: Alignment(0.9, 0.0),
             child: Icon(Icons.delete, color: Colors.white),
@@ -130,48 +131,58 @@ class _MainPageState extends State<HomePage>
         direction: DismissDirection.horizontal,
         child: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: Card(
-            elevation: 4,
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white38,
+                width: 0.5,
+              ),
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(20),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Information(
-                    info: _toDoList[index]["class"],
-                    color: Colors.green,
-                    icon: Icons.school,
-                    isClassField: true,
-                  ),
-                  Information(
-                    info: _toDoList[index]["professor"],
-                    color: Colors.red,
-                    icon: Icons.person_pin,
-                    isClassField: false,
-                  ),
-                  Information(
-                    info: _toDoList[index]["classRom"],
-                    color: Colors.yellow,
-                    icon: Icons.class_,
-                    isClassField: false,
-                  ),
-                  Information(
-                    info: _toDoList[index]["firstHour"],
-                    color: Colors.black,
-                    icon: Icons.watch_later,
-                    isClassField: false,
-                  ),
-                  Information(
-                    info: _toDoList[index]["secondHour"],
-                    color: Colors.blue,
-                    icon: Icons.watch_later,
-                    isClassField: false,
-                  ),
-                ],
+            child: Card(
+              elevation: 0,
+              color: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Information(
+                      info: _toDoList[index]["class"],
+                      color: Colors.green,
+                      icon: Icons.school,
+                      isClassField: true,
+                    ),
+                    Information(
+                      info: _toDoList[index]["professor"],
+                      color: Colors.red,
+                      icon: Icons.person_pin,
+                      isClassField: false,
+                    ),
+                    Information(
+                      info: _toDoList[index]["classRom"],
+                      color: Colors.yellow,
+                      icon: Icons.class_,
+                      isClassField: false,
+                    ),
+                    Information(
+                      info: _toDoList[index]["firstHour"],
+                      color: Colors.black,
+                      icon: Icons.watch_later,
+                      isClassField: false,
+                    ),
+                    Information(
+                      info: _toDoList[index]["secondHour"],
+                      color: Colors.blue,
+                      icon: Icons.watch_later,
+                      isClassField: false,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),

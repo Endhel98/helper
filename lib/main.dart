c8d3e8d8fd31800c4b353731a91745214b8c6e69
endhel <endheltog@gmail.com>
2020-06-05 00:25:39 -0300

    Fixing screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:helper/pages/home_page.dart';
 
-void main() => runApp(MainPage());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MainPage());
+}
 
 class MainPage extends StatelessWidget {
   @override

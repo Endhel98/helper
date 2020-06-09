597304cd3e13d4f32c1e563c97ec96c4a2ecec56
endhel <endheltog@gmail.com>
2020-06-08 22:26:49 -0300

    Update inputField.dart

--- lib/widgets/classPage/inputField.dart
+++ lib/widgets/classPage/inputField.dart
@@ -5,8 +5,10 @@ class InputField extends StatelessWidget {
   final IconData icon;
   final TextEditingController controller;
   final String message;
+  final String label;
 
   InputField({
+    this.label,
     this.hint,
     this.icon,
     this.controller,
@@ -15,52 +17,62 @@ class InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.white12,
-            width: 0.8,
+    return Column(
+      children: <Widget>[
+        TextFormField(
+          controller: controller,
+          validator: (value) {
+            if (value.isEmpty) {
+              return message;
+            }
+            return null;
+          },
+          style: TextStyle(
+            color: Colors.white,
           ),
-        ),
-      ),
-      child: TextFormField(
-        controller: controller,
-        validator: (value) {
-          if (value.isEmpty) {
-            return message;
-          }
-          return null;
-        },
-        style: TextStyle(
-          color: Colors.white,
-        ),
-        decoration: InputDecoration(
-          icon: Padding(
-            padding: const EdgeInsets.only(top: 35, bottom: 10),
-            child: Icon(
-              icon,
+          decoration: InputDecoration(
+            prefixIcon: label != null
+                ? Padding(
+                    padding: EdgeInsets.only(top: 20, bottom: 10),
+                    child: Text(
+                      label,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white70,
+                      ),
+                    ),
+                  )
+                : null,
+            icon: hint != null
+                ? Padding(
+                    padding: const EdgeInsets.only(top: 35, bottom: 10),
+                    child: Icon(
+                      icon,
+                      color: Colors.white,
+                    ),
+                  )
+                : null,
+            border: InputBorder.none,
+            hintText: hint,
+            hintStyle: TextStyle(
               color: Colors.white,
+              fontSize: 15,
+            ),
+            contentPadding: EdgeInsets.only(
+              top: 20,
+              right: 30,
+              bottom: 10,
+              left: 5,
+            ),
+            errorStyle: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 0.5,
             ),
           ),
-          border: InputBorder.none,
-          hintText: hint,
-          hintStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 15,
-          ),
-          contentPadding: EdgeInsets.only(
-            top: 35,
-            right: 30,
-            bottom: 10,
-            left: 5,
-          ),
-          errorStyle: TextStyle(
-            color: Colors.white.withOpacity(0.5),
-            letterSpacing: 0.5,
-          ),
         ),
-      ),
+        Divider(color: Colors.white70),
+      ],
     );
   }
 }

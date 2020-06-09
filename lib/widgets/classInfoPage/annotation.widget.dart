ae64109fee83bd8e1fcf4f63cec06400fd285fcc
endhel <endheltog@gmail.com>
2020-06-08 22:26:59 -0300

    Create annotation.widget.dart

---
+++ lib/widgets/classInfoPage/annotation.widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Annotation extends StatelessWidget {
+  final TextEditingController controller;
+
+  Annotation({this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      width: 500,
+      padding: EdgeInsets.only(top: 20, bottom: 20),
+      child: TextField(
+        maxLines: 100,
+        controller: controller,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.2),
+          border: InputBorder.none,
+        ),
+        style: TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
